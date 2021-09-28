8454c88310eb36f3995a49849531d4c6d6c26b92
weihua <1318022435@qq.com>
2021-09-28 23:20:06 +0800

    今日任务

--- src/com/hwt/entity/User.java
+++ src/com/hwt/entity/User.java
@@ -2,25 +2,29 @@ package com.hwt.entity;
 
 public class User implements Comparable<User> {
 
-    private String id;
+    private Long id;
     private String name;
     private Integer age;
 
-    public User(String id, String name, Integer age) {
+    public User(Long id, String name, Integer age) {
         this.id = id;
         this.name = name;
         this.age = age;
     }
 
-    public String getId() {
+    public User() {
+
+    }
+
+    public Long getId(long l) {
         return id;
     }
 
-    public void setId(String id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
-    public String getName() {
+    public String getName(String 张三) {
         return name;
     }
 
@@ -28,7 +32,7 @@ public class User implements Comparable<User> {
         this.name = name;
     }
 
-    public Integer getAge() {
+    public Integer getAge(int i) {
         return age;
     }
 
@@ -38,25 +42,36 @@ public class User implements Comparable<User> {
 
     @Override
     public int compareTo(User o) {
-        return this.age > o.getAge() ? 1 : this.age == o.getAge() ? 0 : -1;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id='" + id + '\'' +
-                ", name='" + name + '\'' +
-                ", age=" + age +
-                '}';
+        return 0;
     }
 
     @Override
     public boolean equals(Object o) {
-        return true;
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (id != null ? !id.equals(user.id) : user.id != null) return false;
+        if (name != null ? !name.equals(user.name) : user.name != null) return false;
+        return age != null ? age.equals(user.age) : user.age == null;
     }
 
     @Override
     public int hashCode() {
-        return 0;
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (age != null ? age.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("User{");
+        sb.append("id=").append(id);
+        sb.append(", name='").append(name).append('\'');
+        sb.append(", age=").append(age);
+        sb.append('}');
+        return sb.toString();
     }
 }
